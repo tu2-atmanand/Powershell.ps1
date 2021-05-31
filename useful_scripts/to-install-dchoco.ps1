ea360488afd44d4e0e2a5e9b14d5b8d28e38ec34
Atmanand <sanketgauns8@gmail.com>
2021-05-31 20:53:26 +0530

    added dchoco installation file

---
+++ useful_scripts/to-install-dchoco.ps1
@@ -0,0 +1,20 @@
+﻿# download and save installation script, then check signature
+$url = 'https://chocolatey.org/install.ps1'
+$outPath = "$env:temp\installChocolatey.ps1"
+Invoke-WebRequest -UseBasicParsing -Uri $url -OutFile $outPath
+
+# test signature
+$result = Get-AuthenticodeSignature -FilePath $outPath
+if ($result.Status -ne 'Valid')
+{
+    Write-Warning "Installation Script Damaged/Malware?"
+    exit 1
+}
+
+# install chocolatey for current user
+$env:ChocolateyInstall='C:\ProgramData\chocoportable'
+
+Start-Process -FilePath powershell -ArgumentList "-noprofile -noExit -ExecutionPolicy Bypass -File ""$outPath""" -Wait
+
+# clean up
+Remove-Item -Path $outPath
